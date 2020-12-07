7493a15f1b64abf15668fb771a9ecbd56b3c4727
lisa <75568869+lisaariyantisys@users.noreply.github.com>
2020-12-07 19:11:24 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# sistemdeteksirokok
-this kind of repo are open for every single account xixi
-//repository ini dibuat guna pemenuhan mata kuliah Sistem Pakar dimana juga bisa diakses kepada siapapun yang berkemungkinan membutuhkan hal serupa. akan kami upload juga paper yang mendukung implementasinya
+Sistem Pendukung Keputusan untuk Diagnosa penyakit Balita menggunakan metode forward chaining
+
+
+#create database db_penyakit 
+#import database db_penyakit.sql from folder database to database db_penyakit
---
+++ footer.php
@@ -0,0 +1,19 @@
+<br>
+<div class="jombotron" >
+  <footer class="bottom" style="background: #1e252f;color: #fff;">
+          <div class="footer-copyright py-3">
+            <span>
+            <a>
+              <em>&copy; Sistem Pakar: KELOMPOK 6</em>
+            </a>
+          </span>
+                    <ul class="nav" style="float:right">
+                        <li class="nav-item">
+                           <a class="nav-link active" target="_blank" href="https://blogbugabagi.blogspot.com"> Teknik Informatika</a>
+                        </li>
+                    </ul>
+          </div>
+  </footer>
+</div>
+</body>
+</html>
